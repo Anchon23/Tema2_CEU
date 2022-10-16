91f629dce3bc583dabdf691c424a02da1d7aa133
Anchon23 <angellopezrouanet@gmail.com>
2022-10-16 19:01:49 +0200

    Mi cuarto commit

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -3,3 +3,8 @@ lista = [18, -3, 8, 7, 24, -5, 8, 11, -27, 22, 4, 7, 10, 6, 13]
 def modificar(l):
     l = list(set(l))
     l.sort(reverse = True)
+
+    l_temporal = []
+    for i in l:
+        if i%2 == 0:
+            l_temporal.append(i)
