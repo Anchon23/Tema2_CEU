c505b7e170484da972e322f1fb3b497c712a14a9
Anchon23 <angellopezrouanet@gmail.com>
2022-10-16 16:23:26 +0200

    Mi primer commit

---
+++ Ejercicio2.py
@@ -0,0 +1,5 @@
+texto_1 = ["un día que el viento soplaba con fuerza#mira como se mueve aquella banderola -dijo un monje#lo que se mueve es el viento -respondió otro monje#ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro"]
+texto_2 = ["Un día que el viento soplaba con fuerza..."
+"- Mira como se mueve aquella banderola -dijo un monje."
+"- Lo que se mueve es el viento -respondió otro monje."
+"- Ni las banderolas ni el viento, lo que se mueve son vuestras mentes -dijo el maestro."]
